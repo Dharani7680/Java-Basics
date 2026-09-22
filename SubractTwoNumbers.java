963bf96841571f85e8c475292e2e66cf0c9c142c
Dasari Dharani Chowdary <d.dharani.21012010@gmail.com>
2026-09-22 15:16:56 +0530

    Add files via upload

---
+++ SubractTwoNumbers.java
@@ -0,0 +1,10 @@
+public class SubractTwoNumbers {
+    public static void main(String[] args) {
+
+        int num1 = 45;
+        int num2 = 40;
+        int difference = num1 - num2;
+        System.out.println("The difference 0f two numberssis " + difference);
+
+    }
+}
