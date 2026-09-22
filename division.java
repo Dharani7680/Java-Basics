e782765b9d6f4cd42317ab2008b8c4d59a8c7e7a
Dasari Dharani Chowdary <d.dharani.21012010@gmail.com>
2026-09-22 15:27:38 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,12 @@
+public class division {
+
+    public static void main(String[] args) {
+
+        int num1 = 100;
+        int num2 = 4;
+        int division = num1 / num2;
+        System.out.println("The division of " + num1 + " and " + num2 + " is " + division);
+
+    }
+
+}
